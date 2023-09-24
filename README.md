35dbeac0c81a6dc04f90f4f30933b2135530bac4
farrakha <farrakha@gmail.com>
2023-09-25 00:04:24 +0300

    Initial scaffolding based on Generic thermostat

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hass-thermostat-fan
+[WIP]
 Home assistant generic thermostat with multi-speed fan support
+
---
+++ custom_components/thermostat_fan/__init__.py
@@ -0,0 +1,5 @@
+"""Thermostant with fan component."""
+from homeassistant.const import Platform
+
+DOMAIN = "thermostat_fan"
+PLATFORMS = [Platform.CLIMATE]
